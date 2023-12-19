7ee38daa9adf17d158479e93a4b29379c6e3985e
Kevin Yuen <90118768+yuenkev@users.noreply.github.com>
2023-12-19 14:01:04 -0500

    Add files via upload

---
+++ 001-Hello-World/block-letters/initials.py
@@ -0,0 +1,16 @@
+# Task 1: KY are my initials
+
+# Task 2: Kevin Y & I like to do screen printing!
+
+# Task 3
+
+print("""
+K   K  Y   Y
+K  K    Y Y
+K K      Y
+K        Y
+K K      Y
+K  K     Y
+K   K    Y
+"""
+)
